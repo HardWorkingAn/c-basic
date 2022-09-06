913f1ba371d5ef0f8584ddf6f101d3113541c81d
HardWorkingAn <agc780@naver.com>
2022-09-06 19:12:42 +0900

    new delete,포인터 오버로딩 & 스마트포인터 ,()펑터, 임시객체 자동형변환

--- OperatorOverloading4/OperatorOverloading4/OperatorOverloading4.cpp
+++ OperatorOverloading4/OperatorOverloading4/OperatorOverloading4.cpp
@@ -31,6 +31,11 @@ public:
         delete[] adr;
     }
     friend ostream& operator<<(ostream& os, const Point& pos);
+    // operator() 을 하기위한 작성
+    Point operator+(const Point& pos1) const
+    {
+        return Point(xpos + pos1.xpos, ypos + pos1.ypos);
+    }
 };
 ostream& operator<<(ostream& os, const Point& pos)
 {
@@ -100,6 +105,114 @@ ostream& operator<<(ostream& os, const Point2& pos)
     os << '[' << pos.xpos << ", " << pos.ypos << ']' << endl;
     return os;
 }
+
+//operator() 펑터(Functor) : 함수처럼 작동되는 클래스
+class Adder
+{
+public:
+    int operator()(const int& n1, const int& n2)
+    {
+        return n1 + n2;
+    }
+    double operator()(const double& n1, const double& n2)
+    {
+        return n1 + n2;
+    }
+    Point operator()(const Point& n1, const Point& n2)
+    {
+        return n1 + n2;
+    }
+};
+// 펑터를 이용한 버블정렬
+class SortRule
+{
+public:
+    virtual bool operator()(int num1, int num2) const = 0; //가상함수화
+};
+
+class AscendingSort : public SortRule
+{
+public:
+    bool operator()(int num1, int num2) const
+    {
+        if (num1 > num2)
+            return true;
+        else
+            return false;
+    }
+};
+class DescendingSort : public SortRule
+{
+public:
+    bool operator()(int num1, int num2) const
+    {
+        if (num1 < num2)
+            return true;
+        else
+            return false;
+    }
+};
+class DataStorage
+{
+private:
+    int* arr;
+    int idx;
+    const int MAX_LEN;
+public:
+    DataStorage(int arrlen) : idx(0), MAX_LEN(arrlen)
+    {
+        arr = new int[MAX_LEN];
+    }
+    void AddData(int num)
+    {
+        if (MAX_LEN <= idx) {
+            cout << "더 이상 저장할 수 없습니다." << endl;
+            return;
+        }
+        arr[idx++] = num;
+    }
+    void ShowAllData()
+    {
+        for (int i = 0; i < idx; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << endl;
+    }
+    void SortData(const SortRule& functor)
+    {
+        for (int i = 0; i < ( idx - 1 ); i++) {
+            for (int j = 0; j < ( idx - 1); j++) {
+                if (functor(arr[j], arr[j + 1])) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+};
+//임시객체 자동형변환
+class Number2
+{
+private:
+    int num;
+public:
+    Number2(int n = 0) : num(n) { cout << "Number2(int n = 0)" << endl; }
+    void ShowNumber() { cout << num << endl; }
+    Number2& operator=(const Number2& ref)
+    {
+        cout << "operator=()" << endl;
+        num = ref.num;
+        return *this;
+    }
+    operator int() // 형 변환 연산자의 오버로딩
+    {
+        cout << "형 변환 " << num << endl;
+        return num;
+    }
+};
+
 int main()
 {
     // operator new 와 delete는 static 함수 이므로 Point 객체가 완성 전이라도 호출이 가능하다.
@@ -127,7 +240,7 @@ int main()
     cout << *sptr1;
     cout << *sptr2;
     cout << *sptr3;
-
+    
     sptr1->SetPos(10, 20);
     sptr2->SetPos(30, 40);
     sptr3->SetPos(50, 60);
@@ -136,6 +249,38 @@ int main()
     cout << *sptr2;
     cout << *sptr3;
 
+    // operator()
+    Adder adder;
+    cout << adder(1, 3) << endl;
+    cout << adder(1.5, 3.2) << endl;
+    cout << adder(Point(3,4), Point(6,8)) << endl;
+    // 펑터를 이용한 버블정렬
+    DataStorage store(5);
+    store.AddData(40);
+    store.AddData(16);
+    store.AddData(73);
+    store.AddData(25);
+    store.AddData(82);
+
+    store.ShowAllData();
+
+    store.SortData(AscendingSort());
+    store.ShowAllData();
+
+    store.SortData(DescendingSort());
+    store.ShowAllData();
+    cout << endl;
+    //임시객체 자동형변환
+    //Number2 num1(100);
+    //Number2 num2(0);
+    //num2 = num1;
+    //num2.ShowNumber();
+    Number2 number(0);
+    number = 30; // 30 이란 숫자는 number의 대입연산자에서 형식이 맞지 않으므로 Number2(30) 으로 임시객체로 변환후 number.operator=(Number(30)) 으로 됨
+    number.ShowNumber();
+
+    Number2 number2 = number + 20; // operator int 를 통해 number 을 숫자로 30으로 변환하고 30 + 20 해서 50으로 연산후 임시객체로 만들어 operator= 작동
+    number2.ShowNumber();
 
     std::cout << "\nEnd Of Main\n";
 }
