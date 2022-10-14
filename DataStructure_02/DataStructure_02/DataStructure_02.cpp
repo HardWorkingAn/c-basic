9eefcbad2fd0c0a8e5c112a3c1a3fe517397433b
HardWorkingAn <agc780@naver.com>
2022-10-14 16:37:38 +0900

    LinkedList 중간삽입 및 정렬

--- DataStructure_02/DataStructure_02/DataStructure_02.cpp
+++ DataStructure_02/DataStructure_02/DataStructure_02.cpp
@@ -1,12 +1,29 @@
 ﻿#include <iostream>
 
 #include "LikedRead.h"
+#include "Point.h"
 
 using namespace std;
 
 #define ARR_MAX 10
+
+int WhoIsPrecede(int d1, int d2)
+{
+	if (d1 < d2)
+		return 0;
+	else
+		return 1;
+}
+int WhoIsPrecedePoint(Point* d1, Point* d2)
+{
+	if (d1->x < d2->x)
+		return 0;
+	else
+		return 1;
+}
 int main()
 {
+	/*
 	node* head = nullptr;
 	node* tail = nullptr;
 	node* cur = nullptr;
@@ -120,18 +137,18 @@ int main()
 		tail->next = newNode2;
 		tail = newNode2;
 		// 문제 4-1 부분 4-2 차이나서 주석처리
-		/* 
-		if (head == nullptr)
-		{
-			head = newNode2;
-			tail = newNode2;
-		}
-		else
-		{
-			newNode2->next = head;
-			head = newNode2;
-		}
-		*/
+		//
+		//if (head == nullptr)
+		//{
+		//	head = newNode2;
+		//	tail = newNode2;
+		//}
+		//else
+		//{
+		//	newNode2->next = head;
+		//	head = newNode2;
+		//}
+		//
 		
 	}
 	printf("\n");
@@ -173,6 +190,170 @@ int main()
 
 			printf("%d 데이터 삭제\n", delNode->data);
 			delete delNode; //현제 delNode 의 포인터 위치에 있는 데이터를 삭제
+		}
+	}
+	*/
+	
+	// 리스트 생성 및 초기화
+	/*
+	LinkedList list;
+	int data;	
+	ListInit(&list);
+	// 정렬의 기준등륵 이걸 사용한다는 것은 comp는 nullptr 아니기 떄문에 무조건 정렬은 되지만 0과 1 바꾸면 역순으로 변경
+	SetSortRule(&list, WhoIsPrecede);
+
+	// 데이터 저장
+	LInsert(&list, 11); LInsert(&list, 22);
+	LInsert(&list, 33); LInsert(&list, 44);
+	LInsert(&list, 55);
+
+	printf("현재 저장된 데이터의 수 : %d\n", LCount(&list));
+	
+	
+
+	//전체 출력
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+		while (LNext(&list, &data))
+		{
+			printf("%d ", data);
+		}
+	}
+	// 3 데이터 삭제
+	if (LFirst(&list, &data))
+	{
+		if (data == 33)
+		{
+			LRemove(&list);
+		}
+		while (LNext(&list, &data))
+		{
+			if (data == 33)
+			{
+				LRemove(&list);
+			}
+		}
+	}
+	printf("\n\n");
+
+	//삭제후 재출력
+	printf("현재 저장된 데이터의 수 : %d\n", LCount(&list));
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+		while (LNext(&list, &data))
+		{
+			printf("%d ", data);;
+		}
+	}
+	printf("\n\n");
+
+	LInsert(&list, 35);
+
+	printf("현재 저장된 데이터의 수 : %d\n", LCount(&list));
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+		while (LNext(&list, &data))
+		{
+			printf("%d ", data);;
+		}
+	}
+	printf("\n\n");
+	*/
+	//문제 04-3 Point 추가 데이터 재사용 04-4
+	
+	LinkedList list;
+	//int data;
+	Point* data;
+	ListInit(&list);
+	Point compPos;
+	SetSortRule(&list, WhoIsPrecedePoint);
+
+	// 데이터 저장
+	data = new Point(); // 저장공간을 만들어 주기위한 할당
+	SetPoint(data, 1, 8);
+	LInsert(&list, data);
+
+	data = new Point();
+	SetPoint(data, 8, 8);
+	LInsert(&list, data);
+
+	data = new Point();
+	SetPoint(data, 4, 3);
+	LInsert(&list, data);
+
+	data = new Point();
+	SetPoint(data, 6, 6);
+	LInsert(&list, data);
+
+	data = new Point();
+	SetPoint(data, 7, 3);
+	LInsert(&list, data);
+	
+
+	printf("현재 저장된 데이터의 수 : %d\n", LCount(&list));
+
+	//전체 출력
+	if (LFirst(&list, &data))
+	{
+		//printf("%d ", data);
+		ShowPoint(data);
+		while (LNext(&list, &data))
+		{
+			//printf("%d ", data);
+			ShowPoint(data);
+		}
+	}
+	// x = 8 인 데이터 삭제
+	SetPoint(&compPos, 8, 0);
+	if (LFirst(&list, &data))
+	{
+		if (PointComp(data,&compPos) == 1)
+		{
+			data = LRemove(&list);
+			delete(data);
+		}
+		while (LNext(&list, &data))
+		{
+			if (PointComp(data, &compPos) == 1)
+			{
+				data = LRemove(&list);
+				delete(data);
+			}
+		}
+	}
+	printf("\n\n");
+
+	//삭제후 재출력
+	printf("현재 저장된 데이터의 수 : %d\n", LCount(&list));
+	if (LFirst(&list, &data))
+	{
+		//printf("%d ", data);
+		ShowPoint(data);
+		while (LNext(&list, &data))
+		{
+			//printf("%d ", data);
+			ShowPoint(data);
+		}
+	}
+	printf("\n\n");
+
+	//중간 삽입 확인
+	data = new Point();
+	SetPoint(data, 5, 2);
+	LInsert(&list, data);
+
+	//전체 출력
+	if (LFirst(&list, &data))
+	{
+		//printf("%d ", data);
+		ShowPoint(data);
+		while (LNext(&list, &data))
+		{
+			//printf("%d ", data);
+			ShowPoint(data);
 		}
 	}
     std::cout << "\nEnd Of Main\n";
--- DataStructure_02/DataStructure_02/LikedRead.cpp
+++ DataStructure_02/DataStructure_02/LikedRead.cpp
@@ -1,1 +1,92 @@
 #include "LikedRead.h"
+
+void ListInit(List* plist) 
+{
+	plist->head = new node();
+	plist->head->next = nullptr;
+	plist->comp = nullptr;
+	plist->numOfData = 0;
+}
+
+
+
+
+void FInsert(List* plist, LData data) 
+{
+	node* newNode = new node();
+	newNode->data = data;
+
+	newNode->next = plist->head->next;
+	plist->head->next = newNode; //현제 더미의 next 값을 newnode를 바꾸기 떄문에 앞쪽으로 추가되는 형식 132p 그림
+
+	(plist->numOfData)++; //더미를 구현해놓고 해드의 next만 변경하기 때문에 떄문에 값이 한곳에서만 증가함
+}
+
+void SInsert(List* plist, LData data) //중간 삽입 또는 
+{
+	node* newNode = new node();
+	node* pred = plist->head;
+	newNode->data = data;
+
+	while (pred->next != nullptr && plist->comp(data,pred->next->data) != 0)
+	{
+		pred = pred->next;
+	}
+	newNode->next = pred->next;
+	pred->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+void LInsert(List* plist, LData data) // ※ cpp에만 선언된 함수는 사용할 때 항상 위에 두자
+{
+	if (plist->comp == nullptr)
+		FInsert(plist, data);
+	else
+		SInsert(plist, data);
+}
+int LFirst(List* plist, LData* pdata) 
+{
+	if (plist->head->next == nullptr)
+		return FALSE; // 첫번째 값이 존재하지 않을 경우 0으로 리턴
+
+	plist->before = plist->head; // before을 head를 가지게 함으로서 before과 head는 같은 포인터를 가짐
+	plist->cur = plist->head->next; // 더미 다음 포인터를 cur에 저장 -> cur는 첫번째 데이터 위치를 의미
+
+	*pdata = plist->cur->data; // 첫번째 노드에 데이터 전달
+	return TRUE;
+}
+int LNext(List* plist, LData* pdata)
+{
+	if (plist->cur->next == nullptr) // First가 되었다면 cur 다음값이 있는지 확인
+		return FALSE;
+
+	plist->before = plist->cur; // 현재 포인터을 before에 저장
+	plist->cur = plist->cur->next; // cur 를 next위치로 이동
+
+	*pdata = plist->cur->data; // 첫번째 노드에 데이터 전달
+	return TRUE;
+}
+
+LData LRemove(List* plist)
+{
+	node* rpos = plist->cur;
+	LData rdata = rpos->data;
+
+	plist->before->next = plist->cur->next; //이전의 리스트 에 연결된 포인터를 현제커서의 다음 주소로 연결
+	plist->cur = plist->before;  //현제 커서를 전으로 조정
+
+	delete(rpos); // 미리 등륵된 제거할 list를 힙메모리 제거
+	(plist->numOfData)--;
+	return rdata; // 제거된 데이터 값을 리턴
+}
+int LCount(List* plist)
+{
+	return plist->numOfData;
+}
+
+void SetSortRule(List* plist, int(*comp)(LData d1, LData d2)) //메인의 WhoIsPrecde를 통해 0인지 1인지 가져온다.
+{
+	plist->comp = comp;
+}
+
